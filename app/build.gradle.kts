fdf1569651de30d7042117ac7864134aa4575dbc
mdoakil902 <159051140+Mdoakil@users.noreply.github.com>
2025-01-21 20:46:37 +0600

    chats fragment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,7 @@ dependencies {
     implementation(libs.firebase.auth)
     implementation(libs.firebase.database)
 
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
@@ -54,8 +55,10 @@ dependencies {
 
     implementation("com.google.android.gms:play-services-auth:21.3.0")
 
+    implementation("de.hdodenhof:circleimageview:3.1.0")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
 
-    
+
 }
 
 
---
+++ app/src/main/java/com/oakil/whatsapp/Adapter/UserAdapter.java
@@ -0,0 +1,65 @@
+package com.oakil.whatsapp.Adapter;
+
+
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.oakil.whatsapp.Models.Users;
+import com.oakil.whatsapp.R;
+
+import java.util.ArrayList;
+
+
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder>{
+    ArrayList<Users> list;
+    Context context;
+
+    public UserAdapter(Context context, ArrayList<Users> list) {
+        this.context = context;
+        this.list = list;
+    }
+
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.sample_show_user, parent, false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Users users = list.get(position);
+        Glide.with(context).load(users.getProfilePic()).placeholder(R.drawable.avatar).into(holder.image);
+        holder.userName.setText(users.getUserName());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return list == null ? 0: list.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView image;
+        TextView userName, lastMessage;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            image = itemView.findViewById(R.id.);
+            userName = itemView.findViewById(R.id.userNameList);
+            lastMessage = itemView.findViewById(R.id.lastMessage);
+        }
+    }
+
+}
